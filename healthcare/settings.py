b9194f39503062cb2ba3211c7d4259ff95f388ec
D-genius <timothydiero254@gmail.com>
2025-05-08 23:23:37 +0300

    Environment params

--- healthcare/settings.py
+++ healthcare/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get('DEBUG', False).lower() == 'true'
 DEBUG = 'True'
-ALLOWED_HOSTS = ['*']
-# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 
  #cryptographic encryption key
 FIELD_ENCRYPTION_KEY = os.getenv('FIELD_ENCRYPTION_KEY')
