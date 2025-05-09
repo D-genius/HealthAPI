6d20bc9aaca34009140e661617d3a6181846a43f
D-genius <timothydiero254@gmail.com>
2025-05-09 03:16:08 +0300

    Fix CSRF

--- healthcare/settings.py
+++ healthcare/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get('DEBUG', False).lower() == 'true'
 DEBUG = 'True'
+
 # PORT = os.environ.get('PORT')
 # ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
